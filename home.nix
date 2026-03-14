bda1fc7b2566ef59ce10cf359a54108a857e8519
morikawa <morimoriyuki552@gmail.com>
2026-03-14 22:30:10 +0900

    add fuzzel

--- home.nix
+++ home.nix
@@ -30,6 +30,7 @@
     # nixai
     wezterm
     alacritty
+    fuzzel
   ];
 
   # Shell
@@ -59,6 +60,24 @@
     #   enableSpawn = true;
     # };
   };
+
+  # fuzzel
+  xdg.configFile."fuzzel/fuzzel.ini".text = ''
+    [main]
+    font=JetBrainsMono Nerd Font:size=12
+    width=40
+    lines=10
+
+    # dracula color scheme
+    [colors]
+    background=282a36dd
+    text=f8f8f2ff
+    match=8be9fdff
+    selection-match=be9fdff
+    selection=44475add
+    selection-text=f8f8f2ff
+    border=bd93f9ff
+  '';
 
   # nvim
   programs.neovim = {
