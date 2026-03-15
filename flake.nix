5e0727a34cb90d93342a8df02dd6bb0934eba260
morikawa <morimoriyuki552@gmail.com>
2026-03-15 19:04:33 +0900

    fix flake.nix indent

--- flake.nix
+++ flake.nix
@@ -28,15 +28,15 @@
     homeConfigurations = {
       myHome = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = import inputs.nixpkgs {
-	  system = "x86_64-linux";
-	  config.allowUnfree = true;
-	};
-	extraSpecialArgs = {
-	  inherit inputs;
-	};
-	modules = [
-	  ./home.nix
-	];
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home.nix
+        ];
       };
     };
   };
