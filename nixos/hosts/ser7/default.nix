30c7e139595bcd6019717d09a87ee57936738ef6
morikawa <morimoriyuki552@gmail.com>
2026-03-23 23:03:06 +0900

    split default.nix to locale.nix and networking.nix

--- nixos/hosts/ser7/default.nix
+++ nixos/hosts/ser7/default.nix
@@ -8,45 +8,16 @@
   imports = [
     ./hardware.nix
     ../../modules/desktop/desktop.nix
+    ../../modules/system/networking.nix
+    ../../modules/system/locale.nix
   ]
   ++ [
     inputs.xremap.nixosModules.default
     inputs.niri.nixosModules.niri
   ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Tokyo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "ja_JP.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ja_JP.UTF-8";
-    LC_IDENTIFICATION = "ja_JP.UTF-8";
-    LC_MEASUREMENT = "ja_JP.UTF-8";
-    LC_MONETARY = "ja_JP.UTF-8";
-    LC_NAME = "ja_JP.UTF-8";
-    LC_NUMERIC = "ja_JP.UTF-8";
-    LC_PAPER = "ja_JP.UTF-8";
-    LC_TELEPHONE = "ja_JP.UTF-8";
-    LC_TIME = "ja_JP.UTF-8";
-  };
-
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = [pkgs.fcitx5-mozc];
-  };
+  networking.hostName = "nixos";
+  networking.wireless.enable = true;
 
   services.xremap = {
     userName = "morikawa";
@@ -103,25 +74,6 @@
     # vim
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -143,18 +95,6 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  # tailscale(VPN)
-  services.tailscale.enable = true;
-  networking.firewall = rec {
-    enable = true;
-    trustedInterfaces = ["tailscale0"];
-    allowedUDPPorts = [ config.services.tailscale.port ];
-
-    # kdeconnect
-    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-    allowedUDPPortRanges = allowedTCPPortRanges;
-  };
 
   # flatpak
   services.flatpak.enable = true;
---
+++ nixos/modules/system/networking.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  services.tailscale.enable = true;
+  networking.firewall = rec {
+    enable = true;
+    trustedInterfaces = ["tailscale0"];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    # kdeconnect
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+}
