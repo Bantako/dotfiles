c80ed09d905d2648efef5c34685ebcba14989b98
morikawa <morimoriyuki552@gmail.com>
2026-03-28 23:54:47 +0900

    update systemPackages

--- nixos/hosts/ser7/default.nix
+++ nixos/hosts/ser7/default.nix
@@ -42,10 +42,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    # git
-    # vim
+    sops
+    age
   ];
 
   # This value determines the NixOS release from which the default
