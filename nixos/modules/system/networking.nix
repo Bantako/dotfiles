304bf21618d05fe58f395fc3d5822b45eb995416
morikawa <morimoriyuki552@gmail.com>
2026-03-29 19:26:51 +0900

    update network settings

--- nixos/modules/system/networking.nix
+++ nixos/modules/system/networking.nix
@@ -28,4 +28,15 @@
     allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
     allowedUDPPortRanges = allowedTCPPortRanges;
   };
+
+  systemd.services."NetworkManager-wait-online".enable = false;
+  systemd.services.disable-offload = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        /run/current-system/sw/bin/ethtool -K enp2s0 tso off gso off gro off
+      '';
+    };
+  };
 }
