5d15df340a5309b0b141db816a88c634b2c4fa33
morikawa <morimoriyuki552@gmail.com>
2026-06-28 12:18:47 +0900

    feat(secrets): Jelu API token を追加

--- nixos/modules/system/sops.nix
+++ nixos/modules/system/sops.nix
@@ -19,6 +19,7 @@ in {
       paperless_token = userReadable // { };
       immich_token = userReadable // { };
       wger_api_token = userReadable // { };
+      jelu_api_token = userReadable // { };
       openrouter_api_key = userReadable // { };
       discord_bot_token = userReadable // { };
       borg_passphrase = { mode = "0400"; };
